8b558bc7cbb7ae2b0bc73ca689e4f8f994950c6a
Marcus Ellested <marcusellested02@gmail.com>
2023-09-28 11:27:10 +0200

    Added java spring for hello world

---
+++ Brewing/src/test/java/beerbrewers/brewing/BrewingApplicationTests.java
@@ -0,0 +1,13 @@
+package beerbrewers.brewing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrewingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
