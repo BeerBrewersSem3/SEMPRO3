f4d78c10047b58c715431cf4e4786e0640df4b83
msundby <114078619+msundby@users.noreply.github.com>
2023-10-26 12:24:56 +0200

    Lidt connection ju

--- Brewing/src/main/java/beerbrewers/brewing/BrewingApplication.java
+++ Brewing/src/main/java/beerbrewers/brewing/BrewingApplication.java
@@ -1,10 +1,13 @@
 package beerbrewers.brewing;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BrewingApplication
 {
+    @Autowired
+    private OpcuaClientConnection connection;
     public static void main(String[] args)
     {
         SpringApplication.run(BrewingApplication.class, args);
---
+++ Brewing/src/main/java/beerbrewers/brewing/OpcuaClientConnection.java
@@ -0,0 +1,40 @@
+package beerbrewers.brewing;
+import org.eclipse.milo.opcua.sdk.client.OpcUaClient;
+import org.eclipse.milo.opcua.sdk.client.api.subscriptions.UaMonitoredItem;
+import org.eclipse.milo.opcua.sdk.client.api.subscriptions.UaSubscription;
+import org.eclipse.milo.opcua.stack.core.AttributeId;
+import org.eclipse.milo.opcua.stack.core.UaException;
+import org.eclipse.milo.opcua.stack.core.security.SecurityPolicy;
+import org.eclipse.milo.opcua.stack.core.types.builtin.DataValue;
+import org.eclipse.milo.opcua.stack.core.types.builtin.NodeId;
+import org.eclipse.milo.opcua.stack.core.types.builtin.Variant;
+import org.eclipse.milo.opcua.stack.core.types.builtin.unsigned.Unsigned;
+import org.eclipse.milo.opcua.stack.core.types.enumerated.MonitoringMode;
+import org.eclipse.milo.opcua.stack.core.types.structured.MonitoredItemCreateRequest;
+import org.eclipse.milo.opcua.stack.core.types.structured.ReadValueId;
+import org.eclipse.milo.opcua.stack.core.types.structured.MonitoringParameters;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.ExecutionException;
+@Component
+public class OpcuaClientConnection {
+
+    private OpcUaClient client;
+    {
+        try {
+            client = OpcUaClient.create(
+                    "opc.tcp://localhost:4840"
+            );
+
+            client.connect().get();
+            System.out.println("Connected");
+
+        } catch (UaException e) {
+            System.out.println("Connection Error");
+        } catch (ExecutionException e) {
+            System.out.println("Execution Error");
+        } catch (InterruptedException e) {
+            System.out.println("Connection Error");
+        }
+    }
+}
