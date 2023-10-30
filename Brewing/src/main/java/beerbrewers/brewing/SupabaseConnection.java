93351659259d7e6a65d590a53f73ecce42e3a760
Andreas Witten Honoré <114078622+andy-the-ape@users.noreply.github.com>
2023-10-30 17:42:04 +0100

    Added supabase connection
    * Created SupabaseConnection.java.
    * Added postgresql dependency in pom.xml
    * Added .idea\workspace.xml to .gitignore

---
+++ Brewing/src/main/java/beerbrewers/brewing/SupabaseConnection.java
@@ -0,0 +1,47 @@
+package beerbrewers.brewing;
+
+import java.sql.*;
+
+public class SupabaseConnection {
+
+    private static SupabaseConnection instance;
+    private final String databaseURL = "db.hbdjtgvmsnqkxcufwgwh.supabase.co";
+    private final String databaseName = "postgres";
+    private final String databasePort = "5432";
+    private final String databaseUser = "postgres";
+    private final String databasePW = "stivpikoghaardeslag";
+    private Connection connection = null;
+
+    private SupabaseConnection() {
+        connectToSupabaseDatabase();
+    }
+
+    //Using Singleton pattern:
+    public static SupabaseConnection getInstance() {
+        if (instance == null) {
+            instance = new SupabaseConnection();
+        }
+        return instance;
+    }
+
+    private void connectToSupabaseDatabase() {
+        try {
+            DriverManager.registerDriver(new org.postgresql.Driver());
+            connection =DriverManager.getConnection(
+                    "jdbc:postgresql://" + databaseURL
+                            +":" + databasePort
+                            +"/" + databaseName
+                            + "?user=" + databaseUser
+                            +"&password=" + databasePW);
+        } catch (SQLException | IllegalArgumentException exception) {
+            exception.printStackTrace(System.err);
+        }
+        finally {
+            if (connection == null) System.exit(-1);
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+}
