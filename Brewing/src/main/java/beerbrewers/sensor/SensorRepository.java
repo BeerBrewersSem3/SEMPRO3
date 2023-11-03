5d24b6a42f5387150cd4a2fcfd0bfffefe66bee7
Andreas Witten Honoré <114078622+andy-the-ape@users.noreply.github.com>
2023-11-03 22:58:48 +0100

    small comment

--- Brewing/src/main/java/beerbrewers/sensor/SensorRepository.java
+++ Brewing/src/main/java/beerbrewers/sensor/SensorRepository.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+/**
+ * Perhaps this class should be generalized and not exist for each Model.
+ */
 @Repository
 public interface SensorRepository extends JpaRepository<Sensor, Long> {
 
