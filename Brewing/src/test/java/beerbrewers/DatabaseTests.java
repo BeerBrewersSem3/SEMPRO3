f6d6637bf94dfed744648dcbdc71618372fda3fe
Marcus Ellested <46597619+Marcus4420@users.noreply.github.com>
2023-11-07 10:18:46 +0100

    First unit test in spring

--- Brewing/src/test/java/beerbrewers/DatabaseTests.java
+++ Brewing/src/test/java/beerbrewers/DatabaseTests.java
@@ -1,6 +1,8 @@
 package beerbrewers;
 
+import beerbrewers.persistence.SupabaseConnection;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.sql.PreparedStatement;
@@ -9,39 +11,14 @@ import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-//@SpringBootTest
-//class DatabaseTests {
-//
-//    @Test
-//    void DatabaseIsConnected() {
-//        SupabaseConnection supabaseConnection = SupabaseConnection.getInstance();
-//        try {
-//            assertTrue(supabaseConnection.getConnection().isValid(5));
-//        } catch (SQLException e) {
-//            fail("Database connection could not yet be established");
-//        }
-//    }
-//
-//    @Test
-//    void canQueryData() {
-//        try {
-//            SupabaseConnection supabaseConnection = SupabaseConnection.getInstance();
-//            PreparedStatement queryStatement = supabaseConnection.getConnection().prepareStatement("SELECT * FROM test_table WHERE id = ?");
-//            queryStatement.setInt(1, 1);
-//            ResultSet sqlReturnValues = queryStatement.executeQuery();
-//            while (sqlReturnValues.next()) {
-//                assertNotNull(sqlReturnValues.getString(1));
-//            }
-//        } catch (SQLException ex) {
-//            fail("Could not retrieve any data. Perhaps the table is empty?");
-//            ex.printStackTrace();
-//        }
-//    }
-//
-//    @Test
-//    void supabaseIsInstanceOfSupabase() {
-//        SupabaseConnection supabaseConnection = SupabaseConnection.getInstance();
-//        assertTrue(supabaseConnection instanceof SupabaseConnection);
-//    }
-//
-//}
+@SpringBootTest
+class DatabaseTests {
+    @Autowired
+    private SupabaseConnection supabaseConnection;
+
+    @Test
+    void canFetchFromTestTable() {
+        assertTrue(supabaseConnection.testQueryForDatabase());
+    }
+
+}
