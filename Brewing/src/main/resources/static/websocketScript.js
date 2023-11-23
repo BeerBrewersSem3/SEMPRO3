f2d16bbeff50b5cbc9e3748b62073325447ff77e
Marcus Ellested <46597619+Marcus4420@users.noreply.github.com>
2023-11-23 11:18:19 +0100

    Added the alpha version of new barrels

--- Brewing/src/main/resources/static/websocketScript.js
+++ Brewing/src/main/resources/static/websocketScript.js
@@ -43,6 +43,49 @@ function startBatch() {
      //stompClient.send("batch/newBatch", {}, JSON.stringify(batchData));
      //console.log(batchData)
 }
+
+const fill = 12;
+
+function beerfill(fill_level, ingredient) {
+    const currSilo = document.getElementById(ingredient);
+    const col = colCalculator(fill_level);
+    const gradient = fill_level;
+    currSilo.style.background = `linear-gradient(to top, ${col} ${gradient}%, white 10%)`;
+    const title = document.getElementById("ingredient_"+ingredient).textContent = ingredient;
+    const placeholder_prompt = "fill_"+ingredient
+    document.getElementById(placeholder_prompt).textContent = fill_level + "%";
+}
+
+
+function colCalculator(fill) {
+    if (fill >= 50) {
+        let r = 255 - ((fill - 50) * 5.1);
+        const g = 255;
+        const b = 0;
+        return `rgb(${r}, ${g}, ${b})`;
+    } else if (fill < 50 && fill > 0) {
+        const r = 255;
+        let g = (fill * 5.1);
+        const b = 0;
+        return `rgb(${r}, ${g}, ${b})`;
+    } else {
+        return `rgb(0, 0, 0)`;
+    }
+
+}
+
+
+function testDrain() {
+    for (let i = 0; i < 100; i++) {
+        setTimeout(() => {       beerfill(i + 1, 'Wheat');
+        }, i * 250); // i * 500 milliseconds delay
+    }
+}
+
+testDrain();
+
+
+
 connectWebSocket();
 
 
