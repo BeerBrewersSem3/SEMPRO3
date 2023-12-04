6cd0f1dfc6e27c2b78e9c26f2fb9747a9b168dde
HenriikRC <hechr16@student.sdu.dk>
2023-12-04 09:59:07 +0100

    Added boolean for useSimulation (or real beer machine) and added H2 database for offline mode

--- Brewing/src/main/java/beerbrewers/opcua/OpcuaClientConnection.java
+++ Brewing/src/main/java/beerbrewers/opcua/OpcuaClientConnection.java
@@ -1,23 +1,15 @@
 package beerbrewers.opcua;
-import beerbrewers.websocket.WebsocketService;
 import org.eclipse.milo.opcua.sdk.client.OpcUaClient;
-import org.eclipse.milo.opcua.sdk.client.api.subscriptions.UaMonitoredItem;
-import org.eclipse.milo.opcua.sdk.client.api.subscriptions.UaSubscription;
-import org.eclipse.milo.opcua.stack.core.AttributeId;
+import org.eclipse.milo.opcua.sdk.client.api.config.OpcUaClientConfigBuilder;
+import org.eclipse.milo.opcua.stack.client.DiscoveryClient;
 import org.eclipse.milo.opcua.stack.core.UaException;
-import org.eclipse.milo.opcua.stack.core.security.SecurityPolicy;
-import org.eclipse.milo.opcua.stack.core.types.builtin.DataValue;
-import org.eclipse.milo.opcua.stack.core.types.builtin.NodeId;
-import org.eclipse.milo.opcua.stack.core.types.builtin.Variant;
-import org.eclipse.milo.opcua.stack.core.types.builtin.unsigned.Unsigned;
-import org.eclipse.milo.opcua.stack.core.types.enumerated.MonitoringMode;
-import org.eclipse.milo.opcua.stack.core.types.structured.MonitoredItemCreateRequest;
-import org.eclipse.milo.opcua.stack.core.types.structured.ReadValueId;
-import org.eclipse.milo.opcua.stack.core.types.structured.MonitoringParameters;
+import org.eclipse.milo.opcua.stack.core.types.structured.EndpointDescription;
+import org.eclipse.milo.opcua.stack.core.util.EndpointUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 @Component
 public class OpcuaClientConnection {
@@ -27,13 +19,22 @@ public class OpcuaClientConnection {
 
     {
         try {
-            client = OpcUaClient.create(
-                    "opc.tcp://localhost:4840"
-            );
-
-            client.connect().get();
-            logger.info("Connected to OPC UA Sever");
-
+            boolean useSimulation = true;
+            if(useSimulation){
+                client = OpcUaClient.create(
+                        "opc.tcp://localhost:4840"
+                );
+                client.connect().get();
+                logger.info("Connected to OPC UA Sever");
+            } else {
+                List<EndpointDescription> endpointDescriptionList = DiscoveryClient.getEndpoints("opc.tcp://192.168.0.122:4840").get();
+                EndpointDescription configPoint = EndpointUtil.updateUrl(endpointDescriptionList.get(0), "192.168.0.122", 4840);
+                OpcUaClientConfigBuilder configBuilder = new OpcUaClientConfigBuilder();
+                configBuilder.setEndpoint(configPoint);
+                this.client = OpcUaClient.create(configBuilder.build());
+                client.connect().get();
+                logger.info("Connected to OPC UA Server");
+            }
         } catch (UaException | InterruptedException e) {
             logger.error("Connection Error (OPC UA Server)");
         } catch (ExecutionException e) {
