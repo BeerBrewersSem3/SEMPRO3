624cfd5c9214d08892bb14e52cd4760d9156664b
HenriikRC <hechr16@student.sdu.dk>
2023-12-05 12:43:43 +0100

    Added min and max machSpeed to frontend batch

--- Brewing/src/main/resources/static/websocketScript.js
+++ Brewing/src/main/resources/static/websocketScript.js
@@ -171,7 +171,8 @@ function getRequired(type) {
         .then(jsonArray => {
             jsonArray.forEach(data => {
                if (data.name == type) {
-                   setRequired(data.barley,data.hops,data.malt,data.wheat,data.yeast,amount)
+                   setRequired(data.barley,data.hops,data.malt,data.wheat,data.yeast,amount);
+                   setMachSpeed(data.minMachSpeed, data.maxMachSpeed,((data.maxMachSpeed-data.minMachSpeed)/2));
                }
             });
         })
@@ -184,6 +185,17 @@ function setRequired(barley,hops,malt,wheat,yeast,amount) {
     document.getElementById("required_malt").innerText = malt * amount;
     document.getElementById("required_wheat").innerText = wheat * amount;
     document.getElementById("required_yeast").innerText = yeast * amount;
+}
+
+function setMachSpeed(minMachSpeed, maxMachSpeed, recommendedMachSpeed){
+    //let batchSpeedInput = document.getElementById("batchSpeed");
+    //batchSpeedInput.setAttribute("min",minMachSpeed);
+    //batchSpeedInput.setAttribute("max",maxMachSpeed);
+    //batchSpeedInput.value = Math.round(recommendedMachSpeed);
+    document.getElementById("minMachSpeed").innerText = minMachSpeed;
+    document.getElementById("maxMachSpeed").innerText = maxMachSpeed;
+
+    document.getElementById("recSpeed").innerText = Math.round(recommendedMachSpeed);
 }
 
 setRequired(0,0,0,0,0,0);
