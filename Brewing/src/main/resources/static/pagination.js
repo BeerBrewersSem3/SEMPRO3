644832d8cc67813c2be18f88b9b076fda2c46346
Lasse Hvilsted <lahvi22@student.sdu.dk>
2023-12-07 10:09:07 +0100

    further changes to pagination

--- Brewing/src/main/resources/static/pagination.js
+++ Brewing/src/main/resources/static/pagination.js
@@ -1,69 +1,76 @@
 
-function fetchHistory(pageNumber, pageSize) {
-    return fetch(`http://localhost:8080/api/v1/batch?pageNo=${pageNumber}&pageSize=${pageSize}`)
-        .then(response => response.json());
-}
 
-async function loadPageArray(pageNumber,pageSize) {
-    const fetched = await fetchHistory(pageNumber,pageSize);
-    pageArray = fetched;
-    console.log(pageArray);
-    console.log(pageArray.data);
-    if(pageArray && pageArray.data) {
-        pageArray = pageArray.data;
-        currentPage = fetched.pageNumber;
-        totalPages = fetched.totalPages;
+    tableSize = document.getElementById("tableSize").value;
+
+    function fetchHistory(pageNumber, pageSize) {
+        return fetch(`http://localhost:8080/api/v1/batch?pageNo=${pageNumber}&pageSize=${pageSize}`)
+            .then(response => response.json());
     }
-}
 
-async function displayTable() {
-    $(".historyTable tbody tr").remove();
-
-    for (const entry of pageArray) {
-        var row = document.createElement('tr');
-        row.innerHTML =
-            '<td>' + entry.batchId + '</td>'+
-            '<td>' + entry.brewName + '</td>'+
-            '<td>' + entry.amount + '</td>'+
-            '<td>' + await formatTime(entry.startTime) + '</td>'+
-            '<td>' + await formatTime(entry.endTime) + '</td>';
-
-        row.addEventListener("click", function () {
-            window.location.href = `historyPageOpen.html?batchId=${entry.batchId}`;
-        });
-
-        $(".historyTable tbody").append(row);
+    async function loadPageArray(pageNumber,pageSize) {
+        const fetched = await fetchHistory(pageNumber,pageSize);
+        pageArray = fetched;
+        console.log(pageArray);
+        console.log(pageArray.data);
+        if(pageArray && pageArray.data) {
+            pageArray = pageArray.data;
+            currentPage = fetched.pageNumber;
+            totalPages = fetched.totalPages;
+        }
     }
-}
 
+    async function displayTable() {
+        $(".historyTable tbody tr").remove();
 
-let currentPage = 0;
-async function fetchAndDisplay(pageNumber,pageSize) {
-    cursorLoadingAnimation();
-    await loadPageArray(pageNumber,pageSize);
-    await displayTable();
-    currentPage = pageNumber;
-    console.log(pageSize);
-    cursorDefault();
-}
+        for (const entry of pageArray) {
+            var row = document.createElement('tr');
+            row.innerHTML =
+                '<td>' + entry.batchId + '</td>'+
+                '<td>' + entry.brewName + '</td>'+
+                '<td>' + entry.amount + '</td>'+
+                '<td>' + await formatTime(entry.startTime) + '</td>'+
+                '<td>' + await formatTime(entry.endTime) + '</td>';
 
-async function nextPage() {
-    console.log(currentPage);
-    if (currentPage < totalPages - 1) {
-        currentPage++;
-        await fetchAndDisplay(currentPage, 10);
+            row.addEventListener("click", function () {
+                window.location.href = `historyPageOpen.html?batchId=${entry.batchId}`;
+            });
+
+            $(".historyTable tbody").append(row);
+        }
     }
-}
 
-async function previousPage() {
-    console.log(currentPage);
-    if (currentPage > 0) {
-        currentPage--;
-        await fetchAndDisplay(currentPage, 10);
+    function updateTableSize() {
+        tableSize = document.getElementById("tableSize").value;
+        fetchAndDisplay(currentPage,tableSize);
     }
-}
 
-function setPageSize() {
-    pageSize = pageArray.size;
-}
-fetchAndDisplay(0,10);
+    document.getElementById("tableSize").addEventListener("change",function () {
+        updateTableSize();
+    });
+
+    currentPage = 0;
+    async function fetchAndDisplay(pageNumber,pageSize) {
+        await loadPageArray(pageNumber,pageSize);
+        await displayTable();
+        currentPage = pageNumber;
+        console.log(pageSize);
+    }
+
+    async function nextPage() {
+        console.log(currentPage);
+        if (currentPage < totalPages - 1) {
+            currentPage++;
+            await fetchAndDisplay(currentPage, tableSize);
+        }
+    }
+
+    async function previousPage() {
+        console.log(currentPage);
+        if (currentPage > 0) {
+            currentPage--;
+            await fetchAndDisplay(currentPage, tableSize);
+        }
+    }
+
+    fetchAndDisplay(0,tableSize);
+
