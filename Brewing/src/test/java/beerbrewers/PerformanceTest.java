eee7935c16c557378fc5a1a5ec7cf59da05889e8
Marcus Ellested <46597619+Marcus4420@users.noreply.github.com>
2023-12-11 14:53:38 +0100

    First performance test

--- Brewing/src/test/java/beerbrewers/PerformanceTest.java
+++ Brewing/src/test/java/beerbrewers/PerformanceTest.java
@@ -10,12 +10,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 public class PerformanceTest {
 
+    private double allRunTimesSum, allRunTimesAverage;
     private Worker worker;
     private Operation operation;
     private Batch batch;
@@ -27,17 +31,29 @@ public class PerformanceTest {
        this.worker = new Worker(1L,"John John", "MyPassword");
        this.operation= new Operation(1L, worker);
        this.batch = new Batch(1L, operation, BrewEnum.IPA, 100L, 100L);
+       this.allRunTimesSum = 0;
     }
 
     @Test
     public void testStartMachine() {
-        // Long batchId, Operation operation, BrewEnum brewName, long amount, long speed, boolean isCompleted,
-        long startTime = System.currentTimeMillis();
-        machineService.setBatchAttributesToMachine(this.batch);
-        machineService.startMachine();
-        long endTime = System.currentTimeMillis();
-        double timeInSeconds = (double) (endTime-startTime) / 1000;
-        System.out.println("Time to run " + timeInSeconds);
+        ArrayList<Double> allRunTimes = new ArrayList<>();
+        for (int i = 1; i <= 10; i++) {
+            long startTime = System.currentTimeMillis();
+            machineService.setBatchAttributesToMachine(this.batch);
+            machineService.startMachine();
+            long endTime = System.currentTimeMillis();
+            double timeInSeconds = (double) (endTime-startTime) / 1000;
+            System.out.println("Iteration #"+(i)+" just ran at " + timeInSeconds + " seconds");
+            allRunTimes.add(timeInSeconds);
+        }
+
+        allRunTimes.forEach((e) -> {
+            this.allRunTimesSum += e;
+        });
+        this.allRunTimesAverage = this.allRunTimesSum / allRunTimes.size();
+        System.out.println("Dataset: " + allRunTimes);
+        System.out.println("Average runtime: " + this.allRunTimesAverage);
+        assertTrue(this.allRunTimesAverage < 1.0);
     }
 }
 
